56e59020afe2284960987c87b241f7c7029a24af
Lazy foxy <shd@pervert.one>
2024-03-08 21:05:29 +0200

    test

--- script.py
+++ script.py
@@ -13,6 +13,6 @@ file_nameJSON = "novelist.json"
 
 
 def ui():
-    text = gr.Textbox(lines=10, label="Text", placeholder="Enter text here ,,, or somewhere.")
+    text = gr.Textbox(lines=10, label="Text", placeholder="Enter text here ,,, or somewhere!!!! t4est test test.")
     text.change(lambda x: params.update({"text": x}), text, None)
     return gr.Group([text])
